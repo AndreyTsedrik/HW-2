c22a5a310694663653b1f564a9dda4870b2b442e
Andrey <awesome.foxe@yandex.ru>
2023-04-12 22:24:25 +0300

    Задача-3

---
+++ Задачи/Задача 3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число ");
+String s = Console.ReadLine();
+int i = int.Parse(s);
+if (i == 6 | i == 7){
+    System.Console.WriteLine("Да");
+}
+else{
+    System.Console.WriteLine("Нет");
+}
