9ff7749d2c4a48fc1eed56fb9b791ae5d86bcd89
Andrey <awesome.foxe@yandex.ru>
2023-04-12 20:25:22 +0300

    Задача-1

---
+++ Задачи/Задача 1/Program.cs
@@ -0,0 +1,6 @@
+﻿int random = new Random().Next(100, 999);
+int number1 = random / 10;
+int number2 = number1 % 10;
+
+Console.WriteLine($"Случайное число {random}");
+Console.WriteLine($"Вторая цифра {number2}");
