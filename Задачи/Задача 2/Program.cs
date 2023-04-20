f0856bcaac538b30e1f1005a11cd777748afd0f7
Andrey <awesome.foxe@yandex.ru>
2023-04-20 22:24:04 +0300

    Task7-9

--- Задачи/Задача 2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿System.Console.WriteLine("Введите число ");
-string s = Console.ReadLine();
-int i = int.Parse(s);
-if (i > 99){
-    System.Console.WriteLine(s[2]);
-}
-else{
-    System.Console.WriteLine("Третьей цыфры нет");
-}
--- Задачи/Задача 3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿System.Console.WriteLine("Введите число ");
-String s = Console.ReadLine();
-int i = int.Parse(s);
-if (i == 6 | i == 7){
-    System.Console.WriteLine("Да");
-}
-else{
-    System.Console.WriteLine("Нет");
-}
